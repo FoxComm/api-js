c415b875ded9883a71ba4ab16491496d6e04017a
Artem <artem@foxcommerce.com>
2017-04-11 14:12:31 +0300

    Add a helper function to check if script was already loaded

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,8 +1,15 @@
+import _ from 'lodash';
+
 export const isBrowser = new Function("try {return this===window;}catch(e){ return false;}");
 
-export function loadScript(path) {
+const scriptExists = (path) => {
+  const scripts = document.getElementsByTagName('script');
+  return _.some(scripts, (script) => script.src === path);
+};
+
+export const loadScript = (path) => {
   return new Promise((resolve, reject) => {
-    if (document === void 0) {
+    if (document === void 0 || scriptExists(path)) {
       return;
     }
 
@@ -13,4 +20,4 @@ export function loadScript(path) {
     script.src = path;
     document.getElementsByTagName('head')[0].appendChild(script);
   });
-}
+};
