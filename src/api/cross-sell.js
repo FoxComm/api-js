0ed363dd81e10db1683743671bda449e76ebaddc
Josh Ferrerra <josh@foxcommerce.com>
2017-05-15 09:57:42 -0700

    update api and docs for anthill customer endpoints and fix train endpoint
    also clean up trailing whitespace

--- src/api/cross-sell.js
+++ src/api/cross-sell.js
@@ -11,25 +11,21 @@ export default class CrossSell {
   /**
    * @private
    * @method crossSellTrain(pointsObj: object): Promise
-   * @param {Object} pointsObj 
-   * 
+   * @param {Object} pointsObj
+   *
    * Sends a collection of "points" to populate a sparse matrix for cross-sell
    * analytics and related product tracking. The POST body is an object with a "points" key
    * and value filled with an array of "Point" objects.
-   * 
+   *
    * This method is intended for Admin use only.
    *
    * @example pointsObj
-   * 
+   *
    * ```
    * {
-   *    "points": [
-   *      {
-   *        "custID": 1,
-   *        "prodID": 4,
-   *        "chanID": 2,
-   *      } 
-   *    ]
+   *    "cust_id": 1,
+   *    "channel_id": 1,
+   *    "prod_ids": [1, 2, 3, 4]
    * }
    * ```
    */
@@ -39,21 +35,21 @@ export default class CrossSell {
 
   /**
    * @method crossSellRelated(productId: Int, channelId: Int): Promise
-   * @param {Int} productId 
-   * @param {Int} channelId 
-   * 
+   * @param {Int} productId
+   * @param {Int} channelId
+   *
    * Returns JSON of what products are similar to the productId in the query params.
    * { "products": [ RelatedProduct ] }
-   * 
-   * Score == 1 means these products were purchased by the exact same set of customers. 
+   *
+   * Score == 1 means these products were purchased by the exact same set of customers.
    * Score == 0 means no customer has purchased both of these
-   * 
+   *
    * @example Request
-   * 
+   *
    * /v1/public/recommend/prod-prod/3?channel=2
-   * 
+   *
    * @example Response
-   * 
+   *
    * ```
    * {
    *    "products": [
@@ -81,22 +77,22 @@ export default class CrossSell {
 
   /**
    * @method crossSellRelatedFull(productId: Int, channelId: Int, size: Int): Promise
-   * @param {Int} productId 
-   * @param {Int} channelId 
-   * @param {Int} size 
-   * 
+   * @param {Int} productId
+   * @param {Int} channelId
+   * @param {Int} size
+   *
    * Returns JSON of what products are similar to the productId in the query params.
    * { "products": [ RelatedProductFull ] }
-   * 
-   * Score == 1 means these products were purchased by the exact same set of customers. 
+   *
+   * Score == 1 means these products were purchased by the exact same set of customers.
    * Score == 0 means no customer has purchased both of these
-   * 
+   *
    * @example Request
-   * 
+   *
    * /v1/public/recommend/prod-prod/3?channel=2
-   * 
+   *
    * @example Response
-   * 
+   *
    * ```
    * {
    *   "products": [
@@ -125,7 +121,7 @@ export default class CrossSell {
    *   },
    *   {
    *     "product": {
-   *        ... 
+   *        ...
    *     },
    *     "score": 0.07715167498104597
    *   }
@@ -140,12 +136,117 @@ export default class CrossSell {
   }
 }
 
+  /**
+   * @method crossSellCustomerRelated(customerId: Int, channelId: Int): Promise
+   * @param {Int} customerId
+   * @param {Int} channelId
+   *
+   * Returns JSON of what products are similar to the products purchased by the
+   * customer in the query params.
+   * { "products": [ RelatedProduct ] }
+   *
+   * Score == 1 means these products were purchased by the exact same set of customers.
+   * Score == 0 means no customer has purchased both of these
+   *
+   * @example Request
+   *
+   * /v1/public/recommend/cust-prod/3?channel=2
+   *
+   * @example Response
+   *
+   * ```
+   * {
+   *    "products": [
+   *      {
+   *        "id": 2,
+   *        "score": 0
+   *      },
+   *      {
+   *        "id": 1,
+   *        "score": 1
+   *      },
+   *      {
+   *        "id": 0,
+   *        "score": 0
+   *      }
+   *    ]
+   * }
+   * ```
+   */
+  crossSellCustomerRelated(customerId, channelId) {
+    const query = `?channel=${channelId}`;
+    const url = `${endpoints.crossSellCustomerRelated}${customerId}${query}`;
+    return this.api.get(url);
+  }
+
+  /**
+   * @method crossSellCustomerRelatedFull(customerId: Int, channelId: Int, size: Int): Promise
+   * @param {Int} customerId
+   * @param {Int} channelId
+   * @param {Int} size
+   *
+   * Returns JSON of what products are similar to the products purchased by the
+   * customer in the query params.
+   * { "products": [ RelatedProductFull ] }
+   *
+   * Score == 1 means these products were purchased by the exact same set of customers.
+   * Score == 0 means no customer has purchased both of these
+   *
+   * @example Request
+   *
+   * /v1/public/recommend/cust-prod/3?channel=2
+   *
+   * @example Response
+   *
+   * ```
+   * {
+   *   "products": [
+   *   {
+   *     "product": {
+   *        "id": 300,
+   *        "productId": 60727,
+   *        "scope": "1",
+   *        "albums": [ { "images": [...], "name": "default" } ],
+   *        "context": "default",
+   *        "currency": "USD",
+   *        "description": "...",
+   *        "retailPrice": "8500",
+   *        "salePrice": "8500",
+   *        "skus": [ "AB9403_310", "AB9403_330", "AB9403_350", "AB9403_370" ],
+   *        "slug": "dynamo-away-replica-jersey",
+   *        "tags": [ "Apparel", "Soccer" ],
+   *        "taxonomies": [
+   *          { "taxonomy": "brand", "taxons": [ [ "Performance" ] ] },
+   *          { "taxonomy": "category", "taxons": [ [ "Apparel" ] ] },
+   *          { "taxonomy": "color", "taxons": [ [ "Black" ], [ "Light Orange" ] ] },
+   *        ],
+   *        "title": "Dynamo Away Replica Jersey"
+   *      },
+   *      "score": 0.129*9944487358058
+   *   },
+   *   {
+   *     "product": {
+   *        ...
+   *     },
+   *     "score": 0.07715167498104597
+   *   }
+   *  ]
+   *}
+   * ```
+   */
+  crossSellCustomerRelatedFull(customerId, channelId, size) {
+    const query = `?channel=${channelId}&size=${size}`;
+    const url = `${endpoints.crossSellCustomerRelatedFull}${customerId}${query}`;
+    return this.api.get(url);
+  }
+}
+
 // @miniclass Point (CrossSell)
-// @field custID: Number
+// @field cust_id: Number
 // CustomerID number
 //
-// @field prodID: Number
-// ProductID number
+// @field prod_ids: Array<Number>
+// ProductID numbers
 //
 // @field chanID: Number
 // Unique Channel ID number
