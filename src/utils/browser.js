1910a9046ea9f897a7ce46f6edc9c756985bc342
Artem <artem@foxcommerce.com>
2017-06-14 15:19:09 +0300

    Fix the Stripe loading

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,19 +1,19 @@
 import _ from 'lodash';
 
-export const isBrowser = () => {
+export function isBrowser() {
   try {
-    return this === window;
+    return window;
   } catch (e) {
     return false;
   }
-};
+}
 
-const scriptExists = (path) => {
+function scriptExists(path) {
   const scripts = document.getElementsByTagName('script');
   return _.some(scripts, script => script.src === path);
-};
+}
 
-export const loadScript = (path) => {
+export function loadScript(path) {
   return new Promise((resolve, reject) => {
     if (document === void 0 || scriptExists(path)) {
       return;
@@ -22,8 +22,7 @@ export const loadScript = (path) => {
     const script = document.createElement('script');
     script.onload = resolve;
     script.onerror = reject;
-
     script.src = path;
     document.getElementsByTagName('head')[0].appendChild(script);
   });
-};
+}
