6662fb7adb82cc5b24f72ab3b83352213dc38d64
Artem <artem@foxcommerce.com>
2017-06-22 13:48:34 +0300

    Add a status code to the error message

--- src/utils/create-error.js
+++ src/utils/create-error.js
@@ -2,6 +2,7 @@ export default function createError(err) {
   const error = new Error(err.message || String(err));
   error.response = err.response;
   error.responseJson = err.response ? err.response.body : err;
+  error.status = err.response ? err.response.statusCode : err.status || err.statusCode;
 
   return error;
 }
